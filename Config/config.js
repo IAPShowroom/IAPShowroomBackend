6f129069d921684d9c8513af7f2747863880a6fb
Jorge Vega <jorge.e.vega6@gmail.com>
2022-03-18 14:12:18 -0400

    adding more scaffolding

---
+++ Config/config.js
@@ -0,0 +1,54 @@
+/**
+ * Configuration file that loads and stores variables used throughout the project.
+ */
+
+//General variables
+const env = process.env;
+const PORT = env.PORT || 8080;
+
+//BigBlueButton API variables
+// const BBB_SECRET = env.BBB_SECRET || '';
+// const MOD_PASSWORD = env.MOD_PASSWORD || '';
+
+//Endpoint parts
+const BBB_HOST = 'iapstream.ece.uprm.edu';
+const SHOWROOM_HOST = 'iapshowroon.ece.uprm.edu';
+
+const showroom_prefix = '/api/showroom';
+const stream_prefix = '/api/meet';
+const auth_prefix = '/api/auth';
+
+//Database variables
+const showroomDBConfig = {
+    db: {
+        host: env.SHOWROOM_DB_HOST || 'test_db_host',
+        port: env.SHOWROOM_DB_PORT || '5432',
+        user: env.SHOWROOM_DB_USER || 'test_db_user',
+        password: env.SHOWROOM_DB_PASSWORD || 'test_db_password',
+        database: env.SHOWROOM_DB_NAME || 'test_db_name'
+    }
+}
+
+const iapDBConfig = {
+    db: {
+        host: env.IAP_DB_HOST || 'test_db_host',
+        port: env.IAP_DB_PORT || '5432',
+        user: env.IAP_DB_USER || 'test_db_user',
+        password: env.IAP_DB_PASSWORD || 'test_db_password',
+        database: env.IAP_DB_NAME || 'test_db_name'
+    }
+}
+
+module.exports = {
+    PORT: PORT,
+    BBB_HOST: BBB_HOST,
+    SHOWROOM_HOST: SHOWROOM_HOST,
+    showroom_prefix: showroom_prefix,
+    stream_prefix: stream_prefix,
+    auth_prefix: auth_prefix,
+    showroomDBConfig: showroomDBConfig,
+    iapDBConfig: iapDBConfig
+    // ,
+    // BBB_SECRET: BBB_SECRET,
+    // MOD_PASSWORD: MOD_PASSWORD
+};
--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var config = require('./Config/config.js');
+// var authEndpoints = require('./Endpoints/Authentication/AuthEndpoints.js');
+
+const express = require('express');
+const app = express();
+const port = config.PORT;
+
+//use function to pass app object so it can be bound to the method functions within each endpoints file
+
+//use app.use function to attach auth middleware that parses through the headers of all incoming requests
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`IAP Showroom API listening on port ${port}`);
 })
