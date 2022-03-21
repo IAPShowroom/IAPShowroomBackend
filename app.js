8b0ff8582d2b08734a7a36da9519317709f78ebe
Jorge Vega <jorge.e.vega6@gmail.com>
2022-03-21 12:40:58 -0400

    added endpoint binding, and general comments

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+/**
+ * Starting point for the IAP Showroom API server. 
+ */
+
 var config = require('./Config/config.js');
-// var authEndpoints = require('./Endpoints/Authentication/AuthEndpoints.js');
+var authEndpoints = require('./Endpoints/Authentication/AuthEndpoints.js');
+var showroomEndpoints = require('./Endpoints/Showroom/ShowroomEndpoints.js');
+var streamingEndpoints= require('./Endpoints/VideoStreaming/VideoStreamingEndpoints.js');
 
 const express = require('express');
 const app = express();
 const port = config.PORT;
 
-//use function to pass app object so it can be bound to the method functions within each endpoints file
-
-//use app.use function to attach auth middleware that parses through the headers of all incoming requests
+//use app.use function to attach auth middleware that parses through the headers of all incoming requests?
 
 //get CORS middleware and attach with app.use?
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Bind the main module routes to their respective routers
+app.use(config.auth_prefix, authEndpoints);
+app.use(config.showroom_prefix, showroomEndpoints);
+app.use(config.bbb_prefix, streamingEndpoints);
 
 app.listen(port, () => {
   console.log(`IAP Showroom API listening on port ${port}`);
 })
+
+/**
+ * Developer Notes:
+ * 
+ * - probably debamos usar Router objects 
+ * --- combine router objects with imported routes (endpoints) from the Endpoints folder? is this the middleware?
+ */
