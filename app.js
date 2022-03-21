68b57533dcd0039602682fa6bcbc24cf6c82bff4
Jorge Vega <jorge.e.vega6@gmail.com>
2022-03-21 19:56:04 -0400

    added more middleware and scaffolding

--- app.js
+++ app.js
@@ -2,28 +2,52 @@
  * Starting point for the IAP Showroom API server. 
  */
 
-var config = require('./Config/config.js');
-var authEndpoints = require('./Endpoints/Authentication/AuthEndpoints.js');
-var showroomEndpoints = require('./Endpoints/Showroom/ShowroomEndpoints.js');
-var streamingEndpoints= require('./Endpoints/VideoStreaming/VideoStreamingEndpoints.js');
-
+const config = require('./Config/config.js');
+const authHandler = require('./Handlers/AuthHandlers.js');
+const logger = require('./Utility/Logger.js')('app');
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
 const app = express();
+const authRouter = express.Router();
+const showroomRouter = express.Router();
+const streamingRouter = express.Router();
+
 const port = config.PORT;
 
 //TODO: CORS middleware
 
 //Bind the main module routes to their respective routers
-app.use(config.auth_prefix, authEndpoints);
-app.use(config.showroom_prefix, showroomEndpoints);
-app.use(config.bbb_prefix, streamingEndpoints);
+app.use(config.auth_prefix, authRouter);
+app.use(config.showroom_prefix, showroomRouter);
+app.use(config.bbb_prefix, streamingRouter);
+
+//Authenticate user
+app.use(authHandler.authenticate);
+
+//Middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(session({ secret: config.session_secret }));
+
+//Catch non-existant URLs
+app.get('*', function(req, res){
+  res.status(400).send('Invalid URL. Sorry for the inconvenience.');
+})
 
 app.listen(port, () => {
-  console.log(`IAP Showroom API listening on port ${port}`);
-})
+  logger.logDebug('IAP Showroom API listening on port' + port);
+});
+
+module.exports = {
+  authRouter: authRouter,
+  showroomRouter: showroomRouter,
+  streamingRouter: streamingRouter
+}
 
 /**
  * Developer Notes:
  * 
- * - Router objects
+ * - 
  */
