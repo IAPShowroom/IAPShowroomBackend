2d15f84d2de422ef14434a1e360e2e6951cf0272
Jorge Vega <jorge.vega6@upr.edu>
2022-04-12 01:23:54 -0400

    added tests for get meeting info

--- Testing/BBBTesting.js
+++ Testing/BBBTesting.js
@@ -2,23 +2,56 @@
  * Test file for BBB operations.
  */
 
- const testData = require('./TestData.js');
- const streaming = require('../Handlers/VideoStreamingHandlers.js');
- const { logError, logDebug, logTest } = require('../Utility/Logger.js');
+const testData = require('./TestData.js');
+const streaming = require('../Handlers/VideoStreamingHandlers.js');
+const config = require('../Config/config.js');
+const { logError, logDebug, logTest, log } = require('../Utility/Logger.js');
  
- let logCtx = {
-     fileName: 'BBBTesting',
-     fs: ''
- }
+//https://iapstream.ece.uprm.edu/bigbluebutton/api
+var urlPrefix = "https://" + config.BBB_HOST + config.bbb_prefix;
+
+let logCtx = {
+    fileName: 'BBBTesting',
+    fs: ''
+}
  
  
  //run test:
 
- testChecksum();
+//  testChecksum();
+// testGetMeetingInfoURL();
+testGetMeetingInfo();
  
  
  //test functions:
  
+function testGetMeetingInfo () {
+    logCtx.fn = 'testGetMeetingInfo';
+    logTest("Start test", logCtx);
+    var projectID = 1;
+    streaming.getMeetingInfo(projectID, (error, result) => {
+        if (error) logError(error, logCtx);
+        logTest("result: ", logCtx);
+        console.log(result);
+        logTest("End test", logCtx);
+    });
+}
+
+function testGetMeetingInfoURL () {
+    logCtx.fn = 'testGetMeetingInfoURL';
+    logTest("Start test", logCtx);
+    //Construct URL for BBB API call
+    var queryParams = {
+        meetingID: "01",
+    }
+    var queryString = (new URLSearchParams(queryParams)).toString();
+    var checksum = streaming.generateChecksum('getMeetingInfo', queryString);
+    var url = urlPrefix + "/getMeetingInfo?" + queryString + "&checksum=" + checksum;
+    // var checksum = streaming.getMeetingInfo(callName, queryString); //Not yet
+    logTest("url: " + url, logCtx);
+    logTest("End test", logCtx);
+}
+
 function testChecksum () {
     logCtx.fn = 'testChecksum';
     logTest("Start test", logCtx);
