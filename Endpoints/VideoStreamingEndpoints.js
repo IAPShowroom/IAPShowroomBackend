e50b0bb156794206bd28467b0d37df69764e324e
Jorge Vega <jorge.vega6@upr.edu>
2022-04-20 00:24:24 -0400

    commented unused endpoints

--- Endpoints/VideoStreamingEndpoints.js
+++ Endpoints/VideoStreamingEndpoints.js
@@ -8,13 +8,13 @@
  const auth = require('../Handlers/AuthHandlers.js');
   
  //Routes
- var createRoom = '/create';
+//  var createRoom = '/create';
  var joinRoom = '/join';
  var endRoom = '/end';
  var joinStage = '/join-stage';
 //  var history = '/meet-history';
  
- streamingRouter.post(createRoom, auth.authorizeAdmin, streamingHandler.createRoom);
+//  streamingRouter.post(createRoom, auth.authorizeAdmin, streamingHandler.createRoom);
  streamingRouter.post(joinRoom, auth.authenticate, streamingHandler.joinRoom);
  streamingRouter.post(joinStage, auth.authenticate, streamingHandler.joinStage);
  streamingRouter.post(endRoom, auth.authorizeAdmin, streamingHandler.endRoom);
