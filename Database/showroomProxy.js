ed588df90e19418e3424a69b0dbb2df2bb413021
Jorge Vega <jorge.vega6@upr.edu>
2022-04-29 13:38:41 -0400

    updated get events to filter by current day's date

--- Database/showroomProxy.js
+++ Database/showroomProxy.js
@@ -479,7 +479,7 @@ function createEvents (eventList, callback) {
 
 function getEvents(byDate, upcoming, time, date, callback) {
     logCtx.fn = 'getEvents';
-    var getAll = "select * from iap_events where isdeleted = false order by starttime asc";
+    var getAll = "select * from iap_events where isdeleted = false and e_date = $1 order by starttime asc";
     var getAllByDate = "select * from iap_events where e_date = $1 and isdeleted = false and projectid is not null order by starttime asc"; //project id not null to make sure we only select project events
     var getUpcoming = "select * from iap_events where starttime > $1 and e_date = $2 and isdeleted = false order by starttime asc";
     var query = upcoming ? getUpcoming : byDate ? getAllByDate : getAll;
@@ -508,7 +508,8 @@ function getEvents(byDate, upcoming, time, date, callback) {
     } else if (byDate) {
         dbUtils.makeQueryWithParams(pool, query, [date], callback, queryCb);
     } else {
-        dbUtils.makeQuery(pool, query, callback, queryCb);
+        var currentDate = new Date().toISOString().slice(0,10);
+        dbUtils.makeQueryWithParams(pool, query, [currentDate], callback, queryCb);
     }
 }
 
