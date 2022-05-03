b30df3f8ff6c44c2e670ffbbd8644d8b5929df74
Jorge Vega <jorge.vega6@upr.edu>
2022-05-02 21:20:52 -0400

    updates for websockets and commented things from production

--- WebSocketServer.js
+++ WebSocketServer.js
@@ -3,16 +3,17 @@
  */
 
 const WebSocket = require('ws');
-const HttpsServer = require('https').createServer;
-const fs = require('fs');
+// const HttpsServer = require('https').createServer;
+// const fs = require('fs');
 const config = require('./Config/config.js');
 
-const httpServer = HttpsServer({
-    cert: fs.readFileSync(config.ssl_cert_path),
-    key: fs.readFileSync(config.ssl_key_path)
-});
+// const httpServer = HttpsServer({
+//     cert: fs.readFileSync(config.ssl_cert_path),
+//     key: fs.readFileSync(config.ssl_key_path)
+// });
 
-const wss = new WebSocket.Server({ clientTracking: true, server: httpServer });
+// const wss = new WebSocket.Server({ clientTracking: true, server: httpServer });
+const wss = new WebSocket.Server({ clientTracking: true, port: config.ws_port });
 const { log } = require('./Utility/Logger.js');
 
 let logCtx = {
@@ -51,7 +52,7 @@ wss.on('connection', function (ws, request) {
     });
 });
 
-httpServer.listen(config.ws_port);
+// httpServer.listen(config.ws_port);
 
 module.exports = {
     wss: wss
